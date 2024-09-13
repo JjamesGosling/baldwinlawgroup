2f6741853f659f21bd6527d3855890659ac2bded
David <david.toledo@seoprohub.com>
2024-09-12 17:44:53 -0700

    sidebar fixed

--- src/constants/socialLinks.ts
+++ src/constants/socialLinks.ts
@@ -2,11 +2,13 @@ import { faFacebook, faTwitter } from "@fortawesome/free-brands-svg-icons";
 
 export default [
   {
+    id: 1,
     name: "Facebook",
     href: "https://facebook.com/Baldwin-Mader-Law-Group-100070240384711/",
     icon: faFacebook,
   },
   {
+    id: 2,
     name: "Twitter",
     href: "https://twitter.com/baldwin_mader",
     icon: faTwitter,
