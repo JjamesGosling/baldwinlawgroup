37d607ce90b7f0ab5599c785f649f078c8ffe8f5
David <david.toledo@seoprohub.com>
2024-11-25 23:39:12 +0500

    some changes fixed

--- src/sections/homePage/index.tsx
+++ src/sections/homePage/index.tsx
@@ -9,6 +9,7 @@ import WhyChooseUs from "./WhyChooseUs";
 import Title from "@/components/title";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCheckDouble } from "@fortawesome/free-solid-svg-icons";
+import Link from "next/link";
 
 const HomePage = ({ posts }: any) => {
   return (
@@ -27,12 +28,11 @@ const HomePage = ({ posts }: any) => {
             <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d52872517.59607392!2d-161.691169406869!3d36.018281840171966!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x54eab584e432360b%3A0x1c3bb99243deb742!2sUnited%20States!5e0!3m2!1sen!2s!4v1729017307584!5m2!1sen!2s" width="600"  className="border-0 w-full h-[450px]" allowFullScreen={true} loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
             </article>
             <article className="p-5">\
-            <Title title="Areas We Serve" subTitle="Your Trusted Partner Across the Regio​" />
+            <Title title="Areas We Serve" subTitle="Your Trusted Partner Across the Region​" />
             <p className="text-gray-500 mb-5">Baldwin Mader Law Group proudly serves clients across Southern California, other areas including:</p>
             <ul className="mb-6 grid sm:grid-cols-2 gap-x-4 gap-y-2 pl-5 space-y-2">
     <li className="font-archivo flex items-center gap-4 font-normal text-base text-gray-800">
-    <FontAwesomeIcon icon={faCheckDouble} className="w-4" />
-      House Spring</li>
+    <FontAwesomeIcon icon={faCheckDouble} className="w-4" /> Palm Springs</li>
     <li className="font-archivo flex items-center gap-4 font-normal text-base text-gray-800">
     <FontAwesomeIcon icon={faCheckDouble} className="w-4" />San Francisco</li>
     <li className="font-archivo flex items-center gap-4 font-normal text-base text-gray-800">
@@ -48,11 +48,21 @@ const HomePage = ({ posts }: any) => {
     <li className="font-archivo flex items-center gap-4 font-normal text-base text-gray-800">
     <FontAwesomeIcon icon={faCheckDouble} className="w-4" />Long Beach</li>
     <li className="font-archivo flex items-center gap-4 font-normal text-base text-gray-800">
-    <FontAwesomeIcon icon={faCheckDouble} className="w-4" />Port Wind</li>
+    <FontAwesomeIcon icon={faCheckDouble} className="w-4" />Portland</li>
 </ul>
 
             </article>
           </div>
+        </div>
+      </section>
+      <section className="py-10 px-20">
+        <div className="container flex items-center justify-center flex-col">
+        <Title title="Baldwin Mader Law Group is currently investigating claims related to Paul Regan and Next Level. If you invested funds through Paul Regan and/or Next Level please contact our firm." className="text-center"  />
+        <div className="flex gap-2 sm:gap-4 flex-wrap">
+              <Link href="/contact-us" className="btn-after !text-black">
+                Contact Us
+              </Link>
+            </div>
         </div>
       </section>
       <Cta />
